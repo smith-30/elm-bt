d3863ba78f023f410bb5657649d623746c327a42
misu <misu@aptpod.co.jp>
2018-09-24 12:47:01 +0900

    update: v1

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: `${__dirname}/src/index.js`,
     output: {
-        path: `${__dirname}/dist`,
+        path: `${__dirname}/docs`,
         filename: 'bundle.js',
         libraryTarget: 'window',
     },
